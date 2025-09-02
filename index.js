80c1ae724c3c203cc82dea7a90ebf40d8ca338a1
Murali <a06228046@gmail.com>
2025-09-02 23:36:13 +0000

    some changes

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { connectDB } from './config/connectDB.js';
 import userRoutes from './routes/user.routes.js';
 import sellerRoutes from './routes/seller.routes.js';
@@ -13,37 +15,47 @@ import { connectCloudinary } from './config/cloudinary.js';
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const allowedOrigins = ['http://localhost:5173','https://quiba.onrender.com']
+
+const allowedOrigins = ['http://localhost:5173', 'https://quiba.onrender.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
-app.use(cors({
-    origin: allowedOrigins,
-    credentials:true
-}))
-app.use(cookieParser())
-
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
-})
-
-
-
-
+app.use(cookieParser());
 
 app.use("/images", express.static("uploads"));
-app.use('/api/user',userRoutes);
-app.use('/api/admin',sellerRoutes);
-app.use('/api/product',productRoutes);
-app.use('/api/cart',cartRoutes);
-app.use('/api/order',orderRoutes);
-app.use('/api/address',addressRoutes);
+
+app.use('/api/user', userRoutes);
+app.use('/api/admin', sellerRoutes);
+app.use('/api/product', productRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/address', addressRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 connectCloudinary();
 
-app.listen(PORT,(req,res)=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
