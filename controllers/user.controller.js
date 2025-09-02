e2ec8ff1b2a0c5eaad158700c496e733f9459275
Murali <a06228046@gmail.com>
2025-09-02 22:25:42 +0000

    some changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -75,7 +75,7 @@ export const loginUser = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "Strict",
+      sameSite: process.env.NODE_ENV === "production" ? "None" : "Strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     res.status(200).json({
